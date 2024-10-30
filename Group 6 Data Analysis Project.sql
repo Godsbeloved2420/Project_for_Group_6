df51f04c32ba93c79b030c44b26b0405e414b937
Miracle Ugochukwu <ugochukwumiracle2017@gmail.com>
2024-10-30 15:16:30 +0100

    Add files via upload

---
+++ Group 6 Data Analysis Project.sql
@@ -0,0 +1,13 @@
+SELECT * FROM My_DataAnalysis_Projects.dbo.candy_distributor_data_dictionary
+
+SELECT * FROM My_DataAnalysis_Projects.dbo.Candy_Factories
+
+SELECT * FROM My_DataAnalysis_Projects.dbo.[Candy_Products (1)]
+
+SELECT * FROM My_DataAnalysis_Projects.dbo.Candy_Sales
+
+SELECT * FROM My_DataAnalysis_Projects.dbo.Candy_Targets
+
+SELECT * FROM My_DataAnalysis_Projects.dbo.uszips1
+
+--------Cleaning of the different tables---------------
